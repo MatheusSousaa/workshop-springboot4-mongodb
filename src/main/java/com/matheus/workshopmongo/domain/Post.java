699cd6840b4d132264a2c6ab24d57dd4d9d1b6c9
Matheus Sousa <theuzin131@gmail.com>
2026-08-28 11:18:29 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/matheus/workshopmongo/domain/Post.java
@@ -0,0 +1,81 @@
+package com.matheus.workshopmongo.domain;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+@Document
+public class Post implements Serializable {
+
+    @Id
+    private String id;
+    private String title;
+    private String body;
+    private Date date;
+    private User author;
+
+    public Post() {}
+
+    public Post(String id, Date date, String title, String body, User author) {
+        this.id = id;
+        this.title = title;
+        this.body = body;
+        this.date = date;
+        this.author = author;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(id, post.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
