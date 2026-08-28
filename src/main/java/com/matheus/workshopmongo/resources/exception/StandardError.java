fae92109c7580dd4d78edb84dc47a48f1b955cdf
Matheus Sousa <theuzin131@gmail.com>
2026-08-28 09:15:30 -0300

    Obtendo um usuário por id

---
+++ src/main/java/com/matheus/workshopmongo/resources/exception/StandardError.java
@@ -0,0 +1,66 @@
+package com.matheus.workshopmongo.resources.exception;
+
+import java.io.Serializable;
+
+public class StandardError implements Serializable {
+
+    private Long timestamp;
+    private Integer status;
+    private String message;
+    private String path;
+    private String error;
+
+    public StandardError() {
+    }
+
+    public StandardError(Long timestamp, Integer status, String message, String path, String error) {
+        this.timestamp = timestamp;
+        this.status = status;
+        this.message = message;
+        this.path = path;
+        this.error = error;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getError() {
+        return error;
+    }
+
+    public void setError(String error) {
+        this.error = error;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+
+}
+
