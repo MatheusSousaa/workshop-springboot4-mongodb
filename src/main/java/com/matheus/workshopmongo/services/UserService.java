5086f58bca59e171393fd30344081d33c4d13e9e
Matheus Sousa <theuzin131@gmail.com>
2026-08-28 09:34:59 -0300

    Inserção de usuário com POST

--- src/main/java/com/matheus/workshopmongo/services/UserService.java
+++ src/main/java/com/matheus/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.matheus.workshopmongo.services;
 
 import com.matheus.workshopmongo.domain.User;
+import com.matheus.workshopmongo.dto.UserDTO;
 import com.matheus.workshopmongo.repository.UserRepository;
 import com.matheus.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj) {
+        return repo.save(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(),  objDto.getName(), objDto.getEmail());
     }
 }
