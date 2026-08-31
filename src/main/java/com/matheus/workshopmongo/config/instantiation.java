f6356166bdb1e0ba18a8ce469eacc6d0546a2c9a
Matheus Sousa <theuzin131@gmail.com>
2026-08-31 08:34:08 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/matheus/workshopmongo/config/instantiation.java
+++ src/main/java/com/matheus/workshopmongo/config/instantiation.java
@@ -3,6 +3,7 @@ package com.matheus.workshopmongo.config;
 import com.matheus.workshopmongo.domain.Post;
 import com.matheus.workshopmongo.domain.User;
 import com.matheus.workshopmongo.dto.AuthorDTO;
+import com.matheus.workshopmongo.dto.CommentDTO;
 import com.matheus.workshopmongo.repository.PostRepository;
 import com.matheus.workshopmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,13 @@ public class instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para Sao Paulo. Abracos!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("2018/03/21"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um otimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1,c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
---
+++ src/main/java/com/matheus/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.matheus.workshopmongo.services;
+
+import com.matheus.workshopmongo.domain.Post;
+import com.matheus.workshopmongo.domain.User;
+import com.matheus.workshopmongo.repository.PostRepository;
+import com.matheus.workshopmongo.repository.UserRepository;
+import com.matheus.workshopmongo.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
